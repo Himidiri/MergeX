0f7588653f7cf0fe5db3084349ffe00f2e35033e
Himidiri Himakanika <himidirihimakanika@gmail.com>
2023-02-27 21:00:36 +0530

    Initial commit
    Co-authored-by: Jeewana Rasidu <jeewana.20210392@iit.ac.lk>
    Co-authored-by: Senul Perera <senul99@gmail.com>
    Co-authored-by: Sathursh Narendiran <sathu2727@gmail.com>
    Co-authored-by: Amaya Gamage <amayauthsarani@gmail.com>

---
+++ Game/Assets/CameraScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public PlayerScript player;
+    public float sensitivity = 200;
+    public float clampAngle = 85f;
+
+    private float verticalRotation;
+    private float horizontalRotation;
+
+    private void Start()
+    {
+        this.verticalRotation = this.transform.localEulerAngles.x;
+        this.horizontalRotation = this.transform.eulerAngles.y;
+    }
+
+    private void Update()
+    {
+        look();
+        Debug.DrawRay(this.transform.position, this.transform.forward * 2, Color.red);
+    }
+
+    private void look() 
+    {
+        float mouseVertical = -Input.GetAxis("Mouse Y");
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+
+        this.verticalRotation += mouseVertical * this.sensitivity * Time.deltaTime;
+        this.horizontalRotation += mouseHorizontal * this.sensitivity * Time.deltaTime;
+
+        this.verticalRotation = Mathf.Clamp(this.verticalRotation, -this.clampAngle, this.clampAngle);
+
+        this.transform.localRotation = Quaternion.Euler(this.verticalRotation, 0f, 0f);
+        this.player.transform.rotation = Quaternion.Euler(0f, this.horizontalRotation, 0f);
+    }
+}
