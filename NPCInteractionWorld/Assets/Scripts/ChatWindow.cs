6462d8248fca6d468faabf8fb4d1bc80ddf22ab2
Himidiri <himidirihimakanika@gmail.com>
2023-04-05 18:34:43 +0530

    Intergrated with Backend

--- NPCInteractionWorld/Assets/Scripts/ChatWindow.cs
+++ NPCInteractionWorld/Assets/Scripts/ChatWindow.cs
@@ -8,15 +8,21 @@ using TMPro;
 public class ChatWindow : MonoBehaviour
 {
     private PlayerInput playerInput;
-    public ScrollRect scrollRect;
+    // public ScrollRect scrollRect;
+    public bool isOpened = false;
 
     public GameObject player;
+    private NPCResponse npcResponse;
 
-    public GameObject chatObjectPrefab;
-    public TMP_Text playerTextPrefab;
-    public TMP_Text npcTextPrefab;
-    public Transform content;
-    public GameObject lastChatObject;
+    // public GameObject chatObjectPrefab;
+    // public TMP_Text playerTextPrefab;
+    // public TMP_Text npcTextPrefab;
+    // public Transform content;
+    // public GameObject lastChatObject;
+
+    
+    public TMP_InputField inputField; // Reference to the TMP_InputField component
+    public TMP_Text chatLog;
 
     public void Start()
     {
@@ -28,12 +34,14 @@ public class ChatWindow : MonoBehaviour
     }
 
     // Show the input window and disable the player input
-    public void Show()
+    public void Show(NPCResponse npcResponse)
     {
+        isOpened = true;
         gameObject.SetActive(true);
         playerInput.enabled = false;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+        this.npcResponse = npcResponse;
     }
 
     // Hide the input window and enable the player input
@@ -43,5 +51,36 @@ public class ChatWindow : MonoBehaviour
         playerInput.enabled = true;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        inputField.text = "";
+        chatLog.text = "";
+        isOpened = false;
+    }
+
+    public void Answer(){
+        StartCoroutine(npcResponse.GenerateResponse(inputField.text));
+        inputField.text = "";
+    }
+
+    void Update()
+    {
+        if(npcResponse != null){
+            string welcome = npcResponse.welcomeMessage;
+
+            if(npcResponse.lastQuery == ""){
+                chatLog.text = welcome;
+                return;
+            }
+
+            string query = string.Format("<color=green>You: </color>{0}", npcResponse.lastQuery);
+            string response;
+
+            if(npcResponse.loading){
+                response = string.Format("<color=red>{0}: </color> Thinking... ", npcResponse.name);
+            }else{
+                response = string.Format("<color=red>{0}: </color>{1}", npcResponse.name, npcResponse.lastResponse);
+            }
+
+            chatLog.text = string.Format("{0}\n\n{1}\n\n{2}", welcome, query, response);
+        }
     }
 }
--- NPCInteractionWorld/Assets/Scripts/NPCIntractable.cs
+++ NPCInteractionWorld/Assets/Scripts/NPCIntractable.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class NPCIntractable : MonoBehaviour
 {
     private ChatWindow chatWindow_; // Reference to the ChatWindow script
-
+    private NPCResponse npcResponse;
     public GameObject chatWindow; // Reference to the chat window object
 
     public void Interact()
     {
+        if (chatWindow_.isOpened){
+            return;
+        }
         Debug.Log("Interact!"); // Log a message to the console
 
-        chatWindow_.Show(); // Show the chat window
+        chatWindow_.Show(npcResponse); // Show the chat window
     }
 
     public void Start()
     {
         chatWindow_ = chatWindow.GetComponent<ChatWindow>(); // Get the ChatWindow component
+        npcResponse = GetComponent<NPCResponse>();
     }
 }
