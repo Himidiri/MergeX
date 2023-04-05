5ec932ec4b8a8942a592ac0538d806751c736a84
Himidiri <himidirihimakanika@gmail.com>
2023-04-05 08:48:56 +0530

    Bug fix in NPCIntractable script

--- NPCInteractionWorld/Assets/Scripts/NPCIntractable.cs
+++ NPCInteractionWorld/Assets/Scripts/NPCIntractable.cs
@@ -4,21 +4,19 @@ using UnityEngine;
 
 public class NPCIntractable : MonoBehaviour
 {
-    private UI_InputWindow inputWindow_; // Reference to the UI_InputWindow script
+    private ChatWindow chatWindow_; // Reference to the ChatWindow script
 
-    public GameObject inputWindow; // Reference to the input window object
+    public GameObject chatWindow; // Reference to the chat window object
 
     public void Interact()
     {
-        ChatBubble.Create(transform.transform, new Vector3(-0.87f, 1.056f, 0f), Quaternion.Euler(0f, 180f, 0f), ChatBubble.IconType.Happy, "Hello"); // Create a chat bubble with the specified properties
-
         Debug.Log("Interact!"); // Log a message to the console
 
-        inputWindow_.Show(); // Show the input window
+        chatWindow_.Show(); // Show the chat window
     }
 
     public void Start()
     {
-        inputWindow_ = inputWindow.GetComponent<UI_InputWindow>(); // Get the UI_InputWindow component
+        chatWindow_ = chatWindow.GetComponent<ChatWindow>(); // Get the ChatWindow component
     }
 }
